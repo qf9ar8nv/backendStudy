560d74cb8e5718825e0fcd8917ccd5932c3ac277
qf9ar8nv <ckguscjf0@gmail.com>
2021-05-26 19:25:02 +0900

    redux count 작성
    * velog용 react-redux 작성.
    * 간단한 state 변경 함수 제작.

--- react/myapp/src/App.js
+++ react/myapp/src/App.js
@@ -1,10 +1,38 @@
+import { useSelector, useDispatch } from "react-redux";
 
 function App() {
-  return (
-    <div className="App">
-      react 시작
-    </div>
-  );
+	const dispatch = useDispatch()
+
+	const { value } = useSelector(state => state.value)
+	const { count } = useSelector(state => state.count)
+
+	const addValue = () => {
+		dispatch({ type: 'increment' })
+	}
+	const subValue = () => {
+		dispatch({ type: 'decrement' })
+	}
+	const resetValue = () => {
+		dispatch({ type: 'reset' })
+	}
+	const pushButton = () => {
+		dispatch({ type: 'push' })
+	}
+
+	return (
+		<div className="App">
+			<div>
+				value: {value}
+			</div>
+			<button onClick={addValue}> + </button>
+			<button onClick={subValue}> - </button>
+			<button onClick={resetValue}> reset </button>
+			<div>
+				count: {count}
+			</div>
+			<button onClick={pushButton}> click </button>
+		</div>
+	);
 }
 
 export default App;
