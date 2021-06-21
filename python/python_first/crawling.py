6a3dcc1b68c641a9c7f5be280ec44433a20aa6f0
qf9ar8nv <ckguscjf0@gmail.com>
2021-06-21 23:21:53 +0900

    selenium 구동
    * selenium 구현
    * webdriver 사용, crawling으로 html 구성 가져오는 것 구현

---
+++ python/python_first/crawling.py
@@ -0,0 +1,21 @@
+from urllib.parse import quote_plus
+from bs4 import BeautifulSoup
+from selenium import webdriver
+
+baseUrl = 'https://www.google.com/search?q='
+plusUrl = input('무엇을 검색할까요? :')
+url = baseUrl + quote_plus(plusUrl)
+
+driver = webdriver.Chrome()
+driver.get(url)
+
+html = driver.page_source
+soup = BeautifulSoup(html)
+
+r = soup.select('.yuRUbf')
+for i in r:
+    print(i.select_one('.LC20lb.DKV0Md').text)
+    print(i.select_one('.iUh30.Zu0yb.qLRx3b.tjvcx').text)
+    print()
+
+driver.close()
