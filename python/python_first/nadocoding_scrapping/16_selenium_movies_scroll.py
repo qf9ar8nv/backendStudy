e24105ea7958589fdde30031464826403bea6d15
qf9ar8nv <ckguscjf0@gmail.com>
2021-06-25 21:00:07 +0900

    비동기 페이지 처리
    * selenium을 이용한 비동기 페이지 소스 처리
    * google의 이미지를 다운받는 코드 작성.

--- python/python_first/nadocoding_scrapping/16_selenium_movies_scroll.py
+++ python/python_first/nadocoding_scrapping/16_selenium_movies_scroll.py
@@ -1,24 +1,3 @@
-# import requests
-# from bs4 import BeautifulSoup
-
-# url = "https://play.google.com/store/movies/top"
-# headers = {
-#     "User-Agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.114 Safari/537.36",
-#     "Accept-Language":"ko-KR,ko"
-#     }
-
-# res = requests.get(url, headers=headers)
-# res.raise_for_status()
-# soup = BeautifulSoup(res.text, 'lxml')
-
-# movies = soup.find_all("div", attrs={"class":"ImZGtf mpg5gc"})
-# print(len(movies))
-
-# for movie in movies:
-#     title = movie.find("div", attrs={"class":"WsMG1c nnK0zc"}).get_text()
-#     print(title)
-
-
 from selenium import webdriver
 browser = webdriver.Chrome()
 browser.maximize_window()
@@ -27,3 +6,46 @@ browser.maximize_window()
 url = "https://play.google.com/store/movies/top"
 browser.get(url)
 
+# 지정한 위치 스크롤 내리기
+# 모니터(해상도) 높이인 1080의 위치로 스크롤 내림
+# browser.execute_script("window.scrollTo(0, 1080)")
+# browser.execute_script("window.scrollTo(0, 2080)")
+
+
+# 화면 가장 아래로 스크롤 내리기
+# browser.execute_script("window.scrollTo(0, document.body.scrollHeight)")
+
+import time
+interval = 2
+
+# 현재 문서 높이 저장
+prev_heigth = browser.execute_script("return document.body.scrollHeight")
+
+# 반복 수행
+while True:
+    browser.execute_script("window.scrollTo(0, document.body.scrollHeight)")
+
+    time.sleep(interval)
+
+    current_height = browser.execute_script("return document.body.scrollHeight")
+    
+    if current_height == prev_heigth:
+        break
+    
+    prev_heigth = current_height
+    
+print("스크롤 완료")
+
+
+
+import requests
+from bs4 import BeautifulSoup
+
+soup = BeautifulSoup(browser.page_source, 'lxml')
+
+movies = soup.find_all("div", attrs={"class":"ImZGtf mpg5gc"})
+print(len(movies))
+
+for movie in movies:
+    title = movie.find("div", attrs={"class":"WsMG1c nnK0zc"}).get_text()
+    print(title)
